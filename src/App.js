44b9ea5612cdfa664e7154bd0babe9737082b199
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-09 19:00:47 -0400

    add getSampleText() & componentWillMount(). retrieves data successfully

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ class App extends Component {
   constructor(props) {
     super(props); 
     this.state = {
-      params: 4, 
+      paras: 4, 
       html: true, 
       text: ''
     }
   }
+
+  componentWillMount() {
+    this.getSampleText(); 
+  }
+
+  getSampleText() {
+    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras + '&html=' + this.state.html)
+      .then((res) => {
+        this.setState({ text: res.data.text }, () => {
+          console.log(this.state);
+        }); 
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <div className="App">
